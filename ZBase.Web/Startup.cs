f0e014414ed58bad25f55f6a92e95032b1cc1e93
张亮 <48906088@qq.com>
2016-11-03 20:13:31 +0800

    wenxiangmu

---
+++ ZBase.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZBase.Web.Startup))]
+namespace ZBase.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
